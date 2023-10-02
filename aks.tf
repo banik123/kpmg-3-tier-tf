a69b344d69aa7464df16739eba8e555e030b25f0
bishalb <bishalb@amdocs.com>
2023-10-02 16:49:02 +0530

    updated secruity

--- aks.tf
+++ aks.tf
@@ -1,5 +1,6 @@
 resource "azuread_group" "aks_administrators" {
   display_name = "${azurerm_resource_group.tier_app.name}-cluster-administrators"
+  security_enabled = true
   description = "Azure AKS Kubernetes administrators."
 }
 
