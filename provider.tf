51c127b167c69b58e2fa8fc7b18c147499674d9d
bishalb <bishalb@amdocs.com>
2023-10-02 14:21:46 +0530

    added backedn as storage account

--- provider.tf
+++ provider.tf
@@ -7,6 +7,12 @@ terraform {
       version = "~>2.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "appstorageaccount7919"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 provider "azurerm" {
     subscription_id = "3120408b-74d6-4408-a4fb-5b5a7d977000"
