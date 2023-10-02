7f003039e6954f1a10ebf4b907bc3a7c1e0dc7ec
bishalb <bishalb@amdocs.com>
2023-10-02 14:38:55 +0530

    added size

--- vm.tf
+++ vm.tf
@@ -3,7 +3,7 @@ resource "azurerm_windows_virtual_machine" "vm" {
   location              = azurerm_resource_group.tier_app.location
   resource_group_name   = azurerm_resource_group.tier_app.name
   network_interface_ids = [azurerm_network_interface.myvm1nic.id]
-  size                  = "Standard_B1s"
+  size                  = "Standard D2s v3"
   admin_username        = "adminuser"
   admin_password        = "Password123!"
 
